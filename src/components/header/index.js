c361029f1382a8d02dd21092f233ef62f7d3ea54
nmandge <mandgenikhil@gmail.com>
2021-04-21 22:51:58 +0530

    icons allignments added

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,7 +6,7 @@ export default class Header extends Component {
     render() {
         return (
             <div className='bg'>
-                <div className='level-right p-2'>
+                {/* <div className='level-right p-2'>
                     <p className='level-item'> <a href='https://facebook.com/mandge/' class="icon ">
                         <i class="fa fa-lg fa-facebook"></i>
                     </a></p>
@@ -21,12 +21,24 @@ export default class Header extends Component {
                     </a></p>
                     <p className='level-item'></p>
 
-                </div>
+                </div> */}
                 <div className='has-text-centered'>
                     <img alt='' className='circular-img' src={Me} />
                     <div className='is-size-3'>
                         Nikhil Mandge
                         </div>
+                         <a href='https://facebook.com/mandge/' class="icon ">
+                        <i class="fa fa-lg fa-facebook"></i>
+                    </a>
+                    <a href='https://instagram.com/nikhil_mandge/' class="icon has-text-danger">
+                        <i class="fa fa-lg fa-instagram"></i>
+                    </a>
+                    <a href='https://linkedin.com/in/nikhil-mandge-91450b112' class="icon has-text-info">
+                        <i class="fa fa-lg fa-linkedin"></i>
+                    </a>
+                    <a href='www.gmail.com' class="icon has-text-dark">
+                        <i class="fa fa-lg fa-google"></i>
+                    </a>
                 </div>
 
             </div>
